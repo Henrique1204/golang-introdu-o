e7f06ba5e16b4760a2e4c173c42ebc87f5f92713
Paulo <pauloh16.pdr@gmail.com>
2022-07-27 20:47:31 -0300

    Utilizando variaveis

--- src/hello.go
+++ src/hello.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	const nome string = "Paulo"
+	const idade int = 20
+	const versao float32 = 1.2
+
+	fmt.Println("Olá, Sr.", nome, ", sua idade é:", idade)
+	fmt.Println("Este programa está na versão", versao)
 }
